8b8b237b369ae05eea4082a15552391805e14946
NT-57 <nemestamas94@gmail.com>
2026-03-12 16:53:13 +0100

    Frontend part completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import Job, { type JobType } from './components/Job'
 
 const App = () => {
+  const [jobs, setJobs] = useState<JobType[]>([])
+
+  useEffect(()=>{
+    fetch("data.json")
+    .then(response => response.json())
+    .then(data => setJobs(data))
+  },[])
+
   return (
     <div>
       <h1>App</h1>
+
+      {jobs.map(job => <Job {...job} />)}
     </div>
   )
 }
---
+++ src/components/Job.tsx
@@ -0,0 +1,44 @@
+export type JobType = {
+    id: number,
+    company: string,
+    logo: string,
+    new: boolean,
+    featured: boolean,
+    position: string,
+    role: string,
+    level: string,
+    postedAt: string,
+    contract: string,
+    location: string,
+    languages: string[],
+    tools: []
+}
+
+const Job = (props: JobType) => {
+
+  const tags:string[] = [props.role, props.level, ...props.languages, ...props.tools]
+
+  return (
+    <div className="jobCard">
+        <div className="logo">
+            <img src={props.logo} alt={props.company} />
+        </div>
+        <div className="details">
+            <strong>{props.company}</strong>
+            {props.new && <span className="new">NEW!</span>}
+            {props.featured && <span className="featured">FEATURED</span>}
+            <h2>{props.position}</h2>
+            <div>
+                <span>{props.postedAt}</span>
+                <span>{props.contract}</span>
+                <span>{props.location}</span>
+            </div>
+        </div>
+        <div className="tags">
+            {tags.map(tag => <button>{tag}</button>)}
+        </div>
+    </div>
+  )
+}
+
+export default Job
